92bf02e6549e35c4a6bb67a61724d1a619b50000
Eren Yavuz <109018781+erenyavuzs@users.noreply.github.com>
2024-07-22 18:07:51 +0300

    Initial commit

---
+++ crypt_1.py
@@ -0,0 +1,29 @@
+from cryptography.fernet import Fernet
+import os
+
+def write_key():
+    """
+    Generates a key and save it
+    """
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+
+def load_key():
+    """
+    Loads the key from the current directory named `key.key`
+    """
+    return open("key.key", "rb").read()
+
+
+def encrypt(filename, key):
+    """
+    Given a filename (str) and key (bytes), it encrypts the file and write it
+    """
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        file_data = file.read()
+    encrypted_data = f.encrypt(file_data)
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
